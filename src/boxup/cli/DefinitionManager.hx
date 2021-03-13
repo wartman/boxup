71b9fc78ef88e7876f9012a0db604e2525249f60
wartman <faxmaster@gmail.com>
2021-03-13 15:06:09 -0600

    Realized this could be simpler

--- src/boxup/cli/DefinitionManager.hx
+++ src/boxup/cli/DefinitionManager.hx
@@ -6,31 +6,53 @@ using haxe.io.Path;
 
 class DefinitionManager {
   final reporter:Reporter;
-  final compiler:Null<DefinitionCompiler>;
+  final loader:Loader;
+  final compiler:Compiler<Definition>;
   final definitions:Map<String, Definition> = [];
 
   public function new(loader, reporter) {
     this.reporter = reporter;
-    compiler = new DefinitionCompiler(loader, reporter);
+    this.loader = loader;
+    this.compiler = new Compiler(
+      reporter,
+      new DefinitionGenerator(),
+      DefinitionValidator.validator
+    );
   }
+
   public function loadDefinition(type:String):Option<Definition> {
-    if (definitions.exists(type)) return Some(definitions.get(type));
+    if (definitions.exists(type)) 
+      return Some(definitions.get(type));
     
-    var def = compiler.load(type);
-
-    switch def {
-      case Some(def):
-        definitions.set(type, def);
+    return switch loader.load(type) {
+      case Some(source): 
+        switch compiler.compile(source) {
+          case Some(def):
+            definitions.set(type, def);
+            Some(def);
+          case None:
+            None;
+        }
       case None:
+        reportNotFound(type);
+        None;
     }
-
-    return def;
   }
 
-  public function getDocumentType(name:String):Option<String> {
-    return switch name.withoutDirectory().split('.') {
+  public function getDocumentType(filename:String):Option<String> {
+    return switch filename.withoutDirectory().split('.') {
       case [_, type, 'box']: Some(type);
       default: None;
     }
+  }
+
+  function reportNotFound(path:String) {
+    reporter.report([
+      new Error('Defintion not found: ${path}', {
+        min: 0,
+        max: 0,
+        file: path
+      })
+    ], new Source(path, ''));
   }
 }
