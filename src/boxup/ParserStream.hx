a84e866faa4b315313446543dce6467a73662898
wartman <faxmaster@gmail.com>
2021-04-05 12:36:18 -0500

    fix to parser stream

--- src/boxup/ParserStream.hx
+++ src/boxup/ParserStream.hx
@@ -3,17 +3,9 @@ package boxup;
 import haxe.ds.Map;
 
 class ParserStream extends AbstractStream<Chunk<Array<Token>>, Chunk<Array<Node>>> {
-  final parsed:Map<String, Result<Array<Node>>> = [];
-  
   public function write(chunk:Chunk<Array<Token>>) {
     forward({
-      result: parsed.exists(chunk.source.filename)
-        ? parsed.get(chunk.source.filename)
-        : chunk.result.map(tokens -> {
-          var nodes = new Parser(tokens).parse();
-          parsed.set(chunk.source.filename, nodes);
-          return nodes;
-        }),
+      result: chunk.result.map(tokens -> new Parser(tokens).parse()),
       source: chunk.source
     });
   }
